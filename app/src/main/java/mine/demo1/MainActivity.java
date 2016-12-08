af0d2e0acf6ab916e1557e711eb3024dd6945860
yingtian648@163.com <yingtian648@163.com>
2016-12-08 17:04:24 +0800

    修改主页点击跳转方式

--- app/src/main/java/mine/demo1/MainActivity.java
+++ app/src/main/java/mine/demo1/MainActivity.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import mine.demo1.R;
 import mine.demo1.calender.CalenderTestActivity;
@@ -24,35 +30,34 @@ import mine.demo1.pulltorefresh.PullToRefreshActivity;
 import mine.demo1.two_dimension_code.TwoDimenCodeActivity;
 import mine.demo1.viewpager_pinchimageview.PagerPinchActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    //按钮
-    private int[] ids = {R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6
-            , R.id.button7, R.id.button8, R.id.button9, R.id.button10, R.id.button11};
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     //跳转页面
-    private Class [] classes = {CalenderTestActivity.class,CheckBoxInRadiogroupActivity.class,CircleProgressbarActivity.class
-    ,FirstActivity.class,Dialog.class,ChooseImgActivity.class,ImageProcessActivity.class,PullToRefreshActivity.class
-    ,TwoDimenCodeActivity.class,PagerPinchActivity.class};
+    private Class[] classes = {CalenderTestActivity.class, CheckBoxInRadiogroupActivity.class, CircleProgressbarActivity.class
+            , FirstActivity.class, Dialog.class, ChooseImgActivity.class, ImageProcessActivity.class, PullToRefreshActivity.class
+            , TwoDimenCodeActivity.class, PagerPinchActivity.class};
+    private String [] demoNames = {"日历","checkbox在radiogroup的事件","圆环进度条","页面跳转","无背景对话框"
+            ,"头像选择","图片分区压缩","上拉加载|下拉刷新","二维码","viewPager中图片_可放大"};
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.mian_lv);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,android.R.id.text1,demoNames);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
 
-        for (int i = 0; i < ids.length; i++) {
-            findViewById(ids[i]).setOnClickListener(this);
-        }
+    private void goToNextActivity(Class activityClass) {
+        Intent intent = new Intent(this, activityClass);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
     }
 
     @Override
-    public void onClick(View v) {
-        for (int i = 0; i < ids.length; i++) {
-            if (v.getId() == ids[i]) {
-                goToNextActivity(classes[i]);
-            }
-        }
-    }
-    private void goToNextActivity(Class activityClass){
-        Intent intent = new Intent(this,activityClass);
-        startActivity(intent);
-        overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        goToNextActivity(classes[position]);
     }
 }
